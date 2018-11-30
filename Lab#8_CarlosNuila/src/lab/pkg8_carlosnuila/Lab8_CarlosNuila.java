6a524361b1c6866dd3fc84861710bd81cab84dea
Carlos Nuila <cnuila14@icloud.com>
2018-11-30 13:47:40 -0600

    Iniciando Repositorio

---
+++ Lab#8_CarlosNuila/src/lab/pkg8_carlosnuila/Lab8_CarlosNuila.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.pkg8_carlosnuila;
+
+/**
+ *
+ * @author Carlos Nuila
+ */
+public class Lab8_CarlosNuila {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
